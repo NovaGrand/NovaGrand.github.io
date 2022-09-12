97eab3aff6b3941c6b1dbec1dad986c9318422db
Darzuma <barryliu_3295@foxmail.com>
2022-09-13 03:49:24 +0800

    .

---
+++ assets/index.495bb889.js
@@ -0,0 +1,5 @@
+import{_ as n,r as t,aN as l,o as p,c as _,b as s,w as a,a as o,g as m,e as d,t as g}from"./index.30c658c1.js";var v="/assets/\u6816\u606F\u9E1F.bc2f9757.jpg";const f={name:"doc-lazy",data(){return{img:v,usage:`<!--\u5BF9 img video src input \u7B49\u5177\u6709 src \u5C5E\u6027\u7684\u5143\u7D20\u90FD\u6709\u6548-->
+<fit ht-xl ht-25 col-25 bevel>
+    <img v-lazy src="...">
+</fit>
+`}}},u=["src"],h=o("h2",{"tag-bar":"",md:"",style:{"margin-top":"40px"}},"\u61D2\u52A0\u8F7D v-lazy",-1);function y(x,b,z,N,e,w){const r=t("fit"),c=t("prism"),i=l("lazy");return p(),_("div",null,[s(r,{style:{height:"300px"},bevel:""},{default:a(()=>[m(o("img",{src:e.img},null,8,u),[[i]])]),_:1}),h,s(c,{style:{"margin-top":"20px"},lang:"html",bevel:""},{default:a(()=>[d(g(e.usage),1)]),_:1})])}var D=n(f,[["render",y]]);export{D as default};
---
+++ assets/傍晚的林中小屋.548b2ab6.js
@@ -0,0 +1,1 @@
+var s="/assets/\u508D\u665A\u7684\u6797\u4E2D\u5C0F\u5C4B.7161deff.jpg";export{s as i};
